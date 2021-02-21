a9452a359d9c1127dd27e718b12f7a58357fe664
zq4175 <417542931@qq.com>
2021-02-22 00:08:59 +0800

    commit

--- index.php
+++ index.php
@@ -1,16 +1,21 @@
 <?php
 
+header("Content-Type: text/html;charset=utf-8");
 set_time_limit(0);
 project_line("E:/work/debug/phar/vendor/laravel");
 
 function project_line($closure_file)
 {
-//    $start = microtime(true);
+    $start = microtime(true);
     $total_line = 0;
     //如果是目录，则遍历
     if(is_dir($closure_file)){
         if($handle = opendir($closure_file)) {
             while(false !== ($file = readdir($handle))){
+                if($file = '.' || $file = '..') {
+                    continue;
+                }
+                echo $file;
                 //如果是目录继续递归
                 if(is_dir($file)){
                     project_line($file);
@@ -26,9 +31,12 @@ function project_line($closure_file)
         $temp_line = count_line($closure_file);
         $total_line +=$temp_line;
     }
-    echo "总行数为".$total_line;
+//    echo "总行数为:".$total_line;
+    echo "xx:".$total_line;
     PHP_EOL;
+    echo "qq：".(microtime(true)-$start);
 //    echo "执行时间为：".(microtime(true)-$start);
+
 }
 
 //计算文件行数
