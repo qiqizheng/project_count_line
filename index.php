42b3346f079b38bf96483bd849332f5ab8035fa0
zq4175 <417542931@qq.com>
2021-02-23 14:11:13 +0800

    update

--- index.php
+++ index.php
@@ -2,58 +2,75 @@
 
 header("Content-Type: text/html;charset=utf-8");
 set_time_limit(0);
-project_line("E:/work/debug/phar/vendor/laravel");
+$url = "D:/work/debug/phar/vendor/facade/";
+$total_line = new total_line();
+$total_line->project_line($url, $url);
 
-function project_line($closure_file)
-{
-    $start = microtime(true);
-    $total_line = 0;
-    //如果是目录，则遍历
-    if(is_dir($closure_file)){
-        if($handle = opendir($closure_file)) {
-            while(false !== ($file = readdir($handle))){
-                if($file = '.' || $file = '..') {
-                    continue;
-                }
-                echo $file;
-                //如果是目录继续递归
-                if(is_dir($file)){
-                    project_line($file);
-                }else{
-                    //检测代码行数
-                    $temp_line = count_line($file);
-                    $total_line +=$temp_line;
+class total_line{
+
+    protected $_line = 0;
+
+    function  project_line($closure_file,$dir)
+    {
+        $start = microtime(true);
+        //如果是目录，则遍历
+        if(is_dir($closure_file)){
+            if($handle = opendir($closure_file)) {
+                while(false !== ($file = readdir($handle))){
+                    if($file == '.' || $file == '..') {
+                        continue;
+                    }
+
+//                如果是目录继续递归
+                    if(is_dir($dir.$file)){
+                        $this->project_line($dir.$file, $dir.$file."/");
+                    }else{
+                        if(trim(strstr($file, "."),".") != 'php'){
+                            continue;
+                        }
+                        //检测代码行数
+                        $temp_line = $this->file_line($dir.$file);
+                        $this->_line +=$temp_line;
+                    }
                 }
             }
+        }else{
+            //检测代码行数
+            $temp_line = $this->file_line($closure_file);
+            $this->_line +=$temp_line;
         }
-    }else{
-        //检测代码行数
-        $temp_line = count_line($closure_file);
-        $total_line +=$temp_line;
-    }
-//    echo "总行数为:".$total_line;
-    echo "xx:".$total_line;
-    PHP_EOL;
-    echo "qq：".(microtime(true)-$start);
-//    echo "执行时间为：".(microtime(true)-$start);
+        echo "总行数为:".$this->_line;
+        echo PHP_EOL;
+        echo "执行时间为：".(microtime(true)-$start);
 
-}
+    }
 
 //计算文件行数
-function count_line($file)
-{
-    if(is_file($file)){
-        $fp = fopen($file,'r');
-        $line = 0;
-        if($fp){
-            while(stream_get_line($fp, 8192, "\n")){
-                $line++;
+    function file_line($file)
+    {
+        if(is_file($file)){
+            $fp = fopen($file,'r');
+            $line = 0;
+            if($fp){
+//                while(stream_get_line($fp, 8192)){
+//                    $line++;
+//                }
+
+                  while (fgets($fp, 4096) !== false){
+                      $line ++;
+                  }
             }
+
+
+            fclose($fp);
+            return $line;
         }
-        fclose($file);
-        return $line;
+        return 0;
     }
-    return 0;
+
+
+
 }
+
 
 ?>
